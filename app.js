d51821b06c51020e5c349acf2d438c8bcfcba58d
Femi Ayodeji <femidotayodeji@gmail.com>
2022-03-04 01:19:40 +0100

    Add CORS

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
-const app = express()
+const body_parser = require('body-parser')
+const app = express().use(body_parser.json())
 
+const cors = require('cors');
 const server =  require('http').createServer(app);
 server.listen(process.env.PORT || 3000);
 
@@ -10,6 +12,15 @@ const io = new Server(server);
 const port = server.address().port;
 
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/public/index.html');
 });
